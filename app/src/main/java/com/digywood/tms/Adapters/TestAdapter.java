455cd1c4da5a84458ae9fab03994def99597f661
pShashank <pshashank88@gmail.com>
2018-04-09 11:44:50 +0530

    ReviewActivity and ScoreActivity

--- app/src/main/java/com/digywood/tms/Adapters/TestAdapter.java
+++ app/src/main/java/com/digywood/tms/Adapters/TestAdapter.java
@@ -9,8 +9,11 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.TextView;
+
+import com.digywood.tms.DBHelper.DBHelper;
 import com.digywood.tms.R;
 import com.digywood.tms.Pojo.SingleTest;
+import com.digywood.tms.ReviewActivity;
 import com.digywood.tms.TestActivity;
 
 import java.util.ArrayList;
@@ -72,9 +75,9 @@ public class TestAdapter extends RecyclerView.Adapter<TestAdapter.MyViewHolder> 
         holder.btn_resume.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-//                Intent i=new Intent(mycontext, TestActivity.class);
-//                mycontext.startActivity(i);
+//                DBHelper dataObj = new DBHelper(mycontext);
+                Intent i=new Intent(mycontext, ReviewActivity.class);
+                mycontext.startActivity(i);
 
             }
         });
--- app/src/main/java/com/digywood/tms/Pojo/SingleGroup.java
+++ app/src/main/java/com/digywood/tms/Pojo/SingleGroup.java
@@ -5,4 +5,30 @@ package com.digywood.tms.Pojo;
  */
 
 public class SingleGroup {
+
+    public String Group_ID,Group_Qnos,Group_pickup;
+
+    public String getGroup_ID() {
+        return Group_ID;
+    }
+
+    public void setGroup_ID(String group_ID) {
+        Group_ID = group_ID;
+    }
+
+    public String getGroup_Qnos() {
+        return Group_Qnos;
+    }
+
+    public void setGroup_Qnos(String group_Qnos) {
+        Group_Qnos = group_Qnos;
+    }
+
+    public String getGroup_pickup() {
+        return Group_pickup;
+    }
+
+    public void setGroup_pickup(String group_pickup) {
+        Group_pickup = group_pickup;
+    }
 }
